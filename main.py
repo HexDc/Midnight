b9464ee1185300c71b8af3f5e7d685e84eb15d77
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-11-28 09:33:38 -0700

    Add copyright notices to all files

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+    # Midnight is a bot written in discord.py originally for use on the SwitchHaxing Discord Server
+    # Copyright (C) 2018  GriffinG1
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU Affero General Public License as published
+    # by the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU Affero General Public License for more details.
+
+    # You should have received a copy of the GNU Affero General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+    # GriffinG1 can be contacted via GitHub (https://github.com/GriffinG1) or on Discord (A Ghost in the Dust#5250)
+
 # import dependencies
 import os
 from discord.ext import commands
