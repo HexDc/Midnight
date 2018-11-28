3c110259af71dd9bb5b56ff7a251d9205235eba2
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-11-28 11:53:20 -0700

    Apply proper licensing for error handling

--- main.py
+++ main.py
@@ -62,7 +62,23 @@ async def globally_block_dms(ctx):
         return False
     return True
 
-# mostly taken from https://github.com/Rapptz/discord.py/blob/async/discord/ext/commands/bot.py
+# mostly taken from https://github.com/Rapptz/discord.py/blob/async/discord/ext/commands/bot.py. Taken directly from https://github.com/nh-server/kurisu and slightly edited. License below.
+   # Copyright {yyyy} {name of copyright owner}
+
+
+   # Licensed under the Apache License, Version 2.0 (the "License");
+   # you may not use this file except in compliance with the License.
+   # You may obtain a copy of the License at
+
+
+       # http://www.apache.org/licenses/LICENSE-2.0
+
+
+   # Unless required by applicable law or agreed to in writing, software
+   # distributed under the License is distributed on an "AS IS" BASIS,
+   # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   # See the License for the specific language governing permissions and
+   # limitations under the License.
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.CommandNotFound):
@@ -98,6 +114,8 @@ async def on_error(event_method, *args, **kwargs):
     if bot.log_channel:
         embed = discord.Embed(description=error_trace)
         await bot.log_channel.send("An error occurred while processing `{}`.".format(event_method), embed=embed)
+        
+# end Apache licensing.
 
 
 @bot.event
