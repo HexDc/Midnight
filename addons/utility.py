9ea682f4ec44cb8e9b4421df9af830834498f813
HexDc <41858208+HexDc@users.noreply.github.com>
2019-03-21 16:24:41 +0900

    Remove some utility
    Content as Title

--- addons/utility.py
+++ addons/utility.py
@@ -99,24 +99,7 @@ class Utility:
             await user.add_roles(role)
         else:
             await user.remove_roles(role)
-            
-    @commands.command(aliases=['tr'])
-    async def togglerole(self, ctx, role=""):
-        """Used to toggle roles. Available: Direct, Spoiler"""
-        role = role.lower()
-        member = ctx.message.author
-        author_roles = member.roles[1:]
-        if role == "direct":
-            await self.role_change(self.bot.direct_role, member)
-            await ctx.send("Toggled the Direct role!")
-            await self.bot.log_channel.send("{}#{} toggled the Direct role.".format(member.name, member.discriminator))
-        elif role == "spoiler" or role == "spoilers":
-            await self.role_change(self.bot.spoiler_role, member)
-            await ctx.send("Toggled the Spoiler role!")
-            await self.bot.log_channel.send("{}#{} toggled the Spoiler role.".format(member.name, member.discriminator))
-        else:
-            return await ctx.send("You forgot to give an input, or gave an unrecognized input. Please try again. Available roles can be seed with `.help togglerole`")
-            
+               
     @commands.command(aliases=['ui', 'user'])
     async def userinfo(self, ctx, user:discord.Member=None, depth=False):
         """Gets the userinfo for a given server member. If given no member, it will pull your own info. If you specify "True" after providing a member, you will see creation date, join date, and account age."""
@@ -152,83 +135,5 @@ class Utility:
         embed.add_field(name="Created at", value="{}".format(ctx.guild.created_at))
         await ctx.send(embed=embed)
         
-    @commands.command(aliases=['fwinfo', 'fi'])
-    async def firmwareinfo(self, ctx, fw=""):
-        """Returns firmware info for any released firmware in the database"""
-        with open('saves/fwinfo.json', 'r') as f:
-                firmware = json.load(f)
-        if not fw:
-            version = {}
-            for i in firmware.keys():
-                version[i.replace(".", "")] = i
-            a = [int(i) for i in version.keys()]
-            max_firm = firmware[version[str(max(a))]]
-            embed = discord.Embed(title="Firmware info for version {}".format(version[str(max(a))]))
-            embed.description = max_firm
-            await ctx.send(embed=embed)
-        else:
-            if not fw in firmware.keys():
-                return await ctx.send("Firmware isn't in database!")
-            else:
-                embed = discord.Embed(title="Firmware info for version {}".format(fw))
-                embed.description = firmware[fw]
-                await ctx.send(embed=embed)
-            
-    @commands.command(aliases=['listfw', 'lfw'])
-    async def listfirmwares(self, ctx):
-        """Lists all available firmwares"""
-        with open('saves/fwinfo.json', 'r') as f:
-            versionlist = json.load(f)
-        embed = discord.Embed(title="Available firmwares", description="")
-        version = {}
-        for i in versionlist.keys():
-            version[i.replace(".", "")] = i
-        a = [int(i) for i in version.keys()]
-        a.sort()
-        for value in a:
-            embed.description += "{}\n".format(version[str(value)])
-        await ctx.send(embed=embed)
-        
-    @commands.command(aliases=['sinfo'])
-    async def serialinfo(self, ctx, str=""):
-        """Allows a user to check if their switch is patched via serial code. Please input it with no hyphens or anything other than the serial code. Giving no input will show a guide on the patched serial codes"""
-        if len(ctx.message.mentions) > 0 or not str:
-            embed = discord.Embed()
-            embed.set_image(url="https://i.imgur.com/5yKNNme.png")
-            return await ctx.send(embed=embed)
-        elif len(str) < 8:
-            return await ctx.send("{} Invalid input!".format(ctx.author.mention))
-        await ctx.message.delete()
-        try:
-            str = str.upper()
-            leading = str[:4]
-            stringIdentifier = str[4:8]
-            if leading == "XAJ1":
-                return await ctx.send("{} Your Switch with leading characters `{}` and identifier `{}` is definitely unpatched! Congrats! <a:blobwave:499090427887222785>".format(ctx.author.mention, leading, stringIdentifier))
-            elif not stringIdentifier.isdigit():
-                return await ctx.send("{} Stop trying to break the bot. (Leading char: `{}` Identifier: `{}`)".format(ctx.author.mention, leading, stringIdentifier))
-            identifier = int(stringIdentifier)
-            low, mid, high = serials[leading]
-            if identifier < low:
-                return await ctx.send("{} Your Switch with leading characters `{}` and identifier `{}` is definitely unpatched! Congrats! <a:blobwave:499090427887222785>".format(ctx.author.mention, leading, stringIdentifier))
-            elif mid < identifier < high:
-                return await ctx.send("{} It is very likely your Switch with leading characters `{}` and identifier `{}` is patched. <a:sadblob:499094080375226380>".format(ctx.author.mention, leading, stringIdentifier))
-            return await ctx.send("{} Your Switch with leading characters `{}` and identifier `{}` is definitely patched. <a:sadblob:499094080375226380>".format(ctx.author.mention, leading, stringIdentifier))
-        except KeyError:
-            return await ctx.send("{} Invalid input!".format(ctx.author.mention))
-            
-    @commands.command()
-    async def guide(self, ctx, guide=""):
-        embed = discord.Embed(title="How to hack your switch")
-        if guide == "noir" or guide == "noirscape" or guide == "evi":
-            embed.description = "Noirscape's guide can be found [here](https://switchguide.xyz)"
-        elif guide == "sdsetup":
-            embed.description = "SDSetup's guide can be found [here](https://guide.sdsetup.com)"
-        elif guide == "plai" or guide == "plailect":
-            embed.description = "Plailect's guide can be found [here](https://switch.hacks.guide) **Note: Not yet updated**"
-        else:
-            embed.description = "Noirscape's guide can be found [here](https://switchguide.xyz)\nSDSetup's guide can be found [here](https://guide.sdsetup.com)"
-            
-    
 def setup(bot):
     bot.add_cog(Utility(bot))
