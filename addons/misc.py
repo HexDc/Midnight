7d1d02cdfc2d5aa3dd74d6e37ef88c97c1104735
HexDc <41858208+HexDc@users.noreply.github.com>
2019-03-21 16:22:41 +0900

    Remove Management Command
    Content as Title

--- addons/misc.py
+++ addons/misc.py
@@ -30,15 +30,6 @@ class Miscellaneous:
         print('Addon "{}" loaded'.format(self.__class__.__name__))
        
     @commands.command()
-    async def bean(self, ctx, *, str=""):
-        """Beans a user"""
-        if not str:
-            str = ctx.author
-        else:
-            str = str.replace('@everyone', '`@everyone`').replace('@here', '`@here`')
-        await ctx.send("{} is now <:banB:423155991706730499><:banE:423155991798874121><:banA:423155991664787456><:banN:423155991497146380><:banE:423155991798874121><:banD:423155991677501440>".format(str))
-        
-    @commands.command()
     async def say(self, ctx, *, str=""):
         """Says a message through the bot"""
         await ctx.message.delete()
